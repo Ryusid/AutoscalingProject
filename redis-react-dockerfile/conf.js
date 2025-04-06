c0489f33fd52df5225f16c2f6a080027a3470073
KORICHI Sid Ahmed <korichisidahmed13@gmail.com>
2025-04-06 14:44:54 +0200

    commenting config.js

--- redis-react-dockerfile/conf.js
+++ redis-react-dockerfile/conf.js
@@ -1,2 +1,6 @@
-export const URL = 'https://21416339-backend.loca.lt' pour une configuration qui expose le backend avec localtunnel, sinon on met @ip 
-#minikube et le port nodePort associe
+export const URL = 'https://21416339-backend.loca.lt' 
+// expose const URL = 'https://192.168.49.2:30001'
+// pour une configuration qui expose le backend avec localtunnel, sinon on met @ip 
+// minikube et le port nodePort associe
+
+
